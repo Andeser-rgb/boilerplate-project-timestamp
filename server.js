aea0d678d9ef75eee3486c1e1f06c603a852890b
Andeser-rgb <andeser@protonmail.com>
2021-07-23 23:51:22 +0200

    I do a lot of errors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/:time", (req, res) => {
   let time = req.params.time;
-  if(/^\d+$/) let time = parseInt(time);
+  if(/^\d+$/.test(time)) time = parseInt(time);
   let date = new Date(req.params.time);
   if(date === "Invalid Date") res.send({error: "Invalid Date"});
   res.send({unix: date.getTime(), utc: date.toUTCString()})
