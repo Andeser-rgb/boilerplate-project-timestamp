eebba47585feadeab7b4811a26a3891f801c24ad
Andeser-rgb <andeser@protonmail.com>
2021-07-23 22:51:00 +0200

    Fixed some errors

--- server.js
+++ server.js
@@ -24,10 +24,11 @@ app.get("/", function (req, res) {
 app.get("/api/:time", function (req, res) {
   let isDate = moment(req.params.time, "YYYY/MM/DD", true).isValid();
   let isUnix = /^-*\d+/.test(req.params.time);
-  if(isUnix || isDate) let date = new Date(req.params.time);
-  if(isUnix) req.send({unix: req.params.time, utc: date});
-  if(idDate)req.send({unix: (date.getTime() / 1000).toFixed(0), utc: date})
-  if(!isUnix && !isDate) req.send(error: "Invalid date");
+  let date = ""
+  if(isUnix || isDate) date = new Date(req.params.time);
+  if(isUnix) res.send({unix: req.params.time, utc: date});
+  if(isDate)res.send({unix: (date.getTime() / 1000).toFixed(0), utc: date})
+  if(!isUnix && !isDate) res.send({error: "Invalid date"});
 });
 
 
