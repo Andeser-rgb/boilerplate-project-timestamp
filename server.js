d963084e122645cdd70f0f854d522c3a2238c8b2
Andeser-rgb <andeser@protonmail.com>
2021-07-23 22:59:05 +0200

    Removed moment dependency

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-var moment = require("moment");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -22,7 +21,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/:time", function (req, res) {
-  let isDate = moment(req.params.time, "YYYY/MM/DD", true).isValid();
+  let isDate = /(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])?|(?:(?:16|[2468][048]|[3579][26])00)?)))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))(\4)?(?:(?:1[6-9]|[2-9]\d)?\d{2})?$/.test(res.params.time);
   let isUnix = /^-*\d+/.test(req.params.time);
   let date = ""
   if(isUnix || isDate) date = new Date(req.params.time);
